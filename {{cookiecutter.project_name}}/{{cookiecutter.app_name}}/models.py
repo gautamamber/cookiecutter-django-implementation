6cd5e44013d2eefb75502090d7886b42e7e67829
gautamamber <ambergautam1@gmail.com>
2019-04-03 17:03:37 +0530

    big fixed and new model added

--- {{cookiecutter.project_name}}/{{cookiecutter.app_name}}/models.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.app_name}}/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+	title = models.CharField(max_length = 100)
+	text = models.TextField()
+
+	class Meta:
+		verbose_name = "Post"
+		verbose_name_plural = "Post"
+
+	def __str__(self):
+		return str(self.title)
