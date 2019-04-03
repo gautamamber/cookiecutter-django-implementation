941e042170041df72615252453b85ab0c03f7fbd
gautamamber <ambergautam1@gmail.com>
2019-04-03 17:48:51 +0530

    Updated

--- {{cookiecutter.project_name}}/{{cookiecutter.app_name}}/urls.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.app_name}}/urls.py
@@ -1,16 +1,12 @@
 from django.conf.urls import include, url
 from django.urls import path
 from . import views 
-from rest_framework_swagger.views import get_swagger_view
 from .views import PostViewSet
 
-
-schema_view = get_swagger_view(title='Pastebin API')
+schema_view = get_swagger_view(title='API')
 urlpatterns = [
 
 	# Team URL
-
-	path('schema/', schema_view),
 	path('', views.index, name = "index"),
 	path('post/', PostViewSet, name = "Post"),
 	
